9107ea9a61cdd05bcebaf7392ee3b406c43903df
halilbahar <halil.s.bahar@gmail.com>
2021-01-10 15:57:01 +0100

    Create navigation in root element

--- frontend/src/root.element.ts
+++ frontend/src/root.element.ts
@@ -1,8 +1,43 @@
-import { customElement, html, LitElement } from 'lit-element';
+import {
+  css,
+  customElement,
+  html,
+  LitElement,
+  TemplateResult
+} from 'lit-element';
+import { NavigationService } from './services/navigation.service';
+import { Match } from 'navigo';
 
 @customElement('app-root')
 export class RootElement extends LitElement {
+  outlet: TemplateResult | null = null;
+
+  constructor() {
+    super();
+
+    const navigationSerice = NavigationService.getInstance();
+
+    navigationSerice.router
+      .on('/', () => {
+        this.outlet = html`<app-home></app-home>`;
+      })
+      .on('/game/:roomId', (match: Match) => {
+        const data = match.data as { [key: string]: string };
+        this.outlet = html`<app-game roomId="${data.roomId}"></app-game>`;
+      })
+      .resolve();
+  }
+
+  static styles = css`
+    :host {
+      display: block;
+      height: 100vh;
+      width: 100vw;
+      background-color: #fafafa;
+    }
+  `;
+
   render() {
-    return html`<p>Hello World</p>`;
+    return html`${this.outlet}`;
   }
 }
