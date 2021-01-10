515365d25f1567f24bfdf9b13000681e9117546d
halilbahar <halil.s.bahar@gmail.com>
2021-01-10 16:55:31 +0100

    List room with controls

--- frontend/src/pages/home.element.ts
+++ frontend/src/pages/home.element.ts
@@ -1,10 +1,10 @@
-import { css, customElement, html, LitElement } from "lit-element";
-import { Room } from "../models/room.model";
-import { ApiService } from "../services/api.service";
+import { css, customElement, html, LitElement } from 'lit-element';
+import { Room } from '../models/room.model';
+import { ApiService } from '../services/api.service';
+import { globalStyle } from '../styles/global';
 
 @customElement('app-home')
 export class HomeElement extends LitElement {
-
   rooms: Room[] = [];
 
   constructor() {
@@ -12,16 +12,44 @@ export class HomeElement extends LitElement {
     this.refreshRooms();
   }
 
-  static styles = css`
-    :host {
-      display: block;
-      height: 100%;
-      width: 100%;
-    }
-  `;
+  static styles = [
+    globalStyle,
+    css`
+      :host {
+        display: block;
+        height: 100%;
+        width: 100%;
+        padding: 3% 5% !important;
+      }
+
+      .controls {
+        display: flex;
+      }
+
+      .controls input {
+        flex: 1;
+      }
+
+      .controls > *:not(:first-child) {
+        margin-left: 1rem;
+      }
+    `
+  ];
 
   render() {
-    return html``;
+    return html`
+      <div class="controls">
+        <input />
+        <button>Join</button>
+        <button>Reload</button>
+      </div>
+
+      ${this.rooms.map(item => html`
+      <div>
+      ${item.id} - ${item.name}
+      </div>
+      `)}
+    `;
   }
 
   private refreshRooms(): void {
