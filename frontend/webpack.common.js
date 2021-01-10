04a8e9e5d12a0109a59e483471edcb44404c09a5
halilbahar <halil.s.bahar@gmail.com>
2021-01-10 14:21:34 +0100

    Rename root element file

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/root-element.ts',
+  entry: './src/root.element.ts',
   module: {
     rules: [
       {
